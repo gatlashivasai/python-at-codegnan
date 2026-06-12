7a3b17d49551e0c737464f591cb015b9196f7ff1
gatlashivasai <shivasaigatlaa@gmail.com>
2026-06-12 16:18:51 +0530

    operators and Strings

---
+++ operators.py
@@ -0,0 +1,75 @@
+Python 3.13.0 (tags/v3.13.0:60403a5, Oct  7 2024, 09:38:07) [MSC v.1941 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+#arithematic operators
+a,b=(10,8)
+print(a+b)
+18
+print(a-b)
+2
+print(a*b)
+80
+print(a/b)
+1.25
+print(a//b)
+1
+print(a%b)
+2
+print(a**b)
+100000000
+#assignment operators
+a=10
+a+=3
+print(a)
+13
+a-=3
+print(a)
+10
+a*=3
+print(a)
+30
+a/=3
+print(a)
+10.0
+a//=3
+print(a)
+3.0
+a**=3
+print(a)
+27.0
+a%=3
+print(a)
+0.0
+>>> #relational operators
+>>> a,b=(10,3)
+>>> print(a<b)
+False
+>>> print(a>b)
+True
+>>> print(a<=b)
+False
+>>> print(a>=b)
+True
+>>> print(a==b)
+False
+>>> print(a!=b)
+True
+>>> #logical operators
+>>> a,b=(10,3)
+>>> print(a<b and a>b)
+False
+>>> print(a>b and a<b)
+False
+>>> print(a==b or a>=b)
+True
+>>> print
+<built-in function print>
+>>> # bitwise operators
+>>> a,b=(10,3)
+>>> print(a|b)
+11
+>>> print(a<<b)
+80
+>>> print(a>>b)
+1
+>>> print(a^b)
+9
